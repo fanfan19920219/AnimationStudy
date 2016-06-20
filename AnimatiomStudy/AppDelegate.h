40bd6025e20fcf11e0f637e573eab27d57271ffb
zhangzhihua19920219 <406608969@qq.com>
2016-06-20 13:36:01 +0800

    Initial Commit

---
+++ AnimatiomStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimatiomStudy
+//
+//  Created by zhangzhihua on 16/6/20.
+//  Copyright © 2016年 zhangzhihua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimatiomStudy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimatiomStudy
+//
+//  Created by zhangzhihua on 16/6/20.
+//  Copyright © 2016年 zhangzhihua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
